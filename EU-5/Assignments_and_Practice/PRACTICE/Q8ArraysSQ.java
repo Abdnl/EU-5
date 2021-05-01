92cc310dc4b3ca96da789fe554d3d920bba5d354
Abdurrahman Iscan <abdurrahmaniscan@gmail.com>
2021-05-01 22:19:12 +0200

    EU-5

---
+++ EU-5/Assignments_and_Practice/PRACTICE/Q8ArraysSQ.java
@@ -0,0 +1,18 @@
+package PRACTICE;
+
+public class Q8ArraysSQ {
+
+	public static void main(String[] args) {
+		
+		
+		int arr[]= {1,2,3,4};
+		int i =0;
+		do {
+			System.out.println(arr[i]+" ");
+			i++;
+		}while (i<arr.length -1);
+			
+		}
+
+	}
+
--- EU-5/LiveReviewWithMuhtar/utilities/MathUtility.java
+++ EU-5/LiveReviewWithMuhtar/utilities/MathUtility.java
@@ -1,6 +1,6 @@
 package utilities;
 
-public class MathUtulity {
+public class MathUtility {
 
 	public static int max(int a, int b) {
 		return (a >= b) ? a : b;
