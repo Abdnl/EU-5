169c1228ae2e689813744af0733b1f4bb82f5994
Abdurrahman Iscan <abdurrahmaniscan@gmail.com>
2021-05-22 19:53:33 +0200

    EU-5

---
+++ EU-5/LiveReviewWithMuhtar/LiveReviewWithMuhtar9_22_05_2021/CircleObjects.java
@@ -0,0 +1,24 @@
+package LiveReviewWithMuhtar9_22_05_2021;
+
+public class CircleObjects {
+
+	public static void main(String[] args) {
+
+		Circle circle = new Circle(5);
+		// circle.radius = -1;
+		/// System.out.println(circle.radius);
+
+		// circle.setRadius(-1);
+		System.out.println(circle);
+
+		System.out.println("----------------------");
+
+		Circle circle2 = new Circle(1);
+		System.out.println(circle2);
+
+		circle2.setRadius(2);
+		System.out.println(circle2);
+
+	}
+
+}
